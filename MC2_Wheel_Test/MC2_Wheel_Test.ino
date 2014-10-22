bc09119c486a85dbfec995417daed6550d481f00
Mike Helbig <helbig.mike@gmail.com>
2014-10-21 21:41:59 -0500

    Added wire color legend for controller wiring harness.

--- MC2_Wheel_Test/MC2_Wheel_Test.ino
+++ MC2_Wheel_Test/MC2_Wheel_Test.ino
@@ -17,6 +17,12 @@ char keys[ROWS][COLS] = {
   {'V','1','4','D'},
   {'U','3','J','L'}
 };
+
+// Mad Catz MC2 Steering wheel wire color matrix connections
+// Columns: 2=BLK; 3=GRY; 4=PNK; 5=PUR
+// Rows:    6=RED; 7=ORN; 8=YEL; 9=BRN
+// Motors:  PWR=GRN; LEFT=WHT; RIGHT=BLU
+
 byte rowPins[ROWS] = {6, 7, 8, 9}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {2, 3, 4, 5}; //connect to the column pinouts of the keypad
 
