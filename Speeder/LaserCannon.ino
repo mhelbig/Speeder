af9c0716ba8ff0e43f03fd36fb5b1bda3466e0c8
Mike Helbig <helbig.mike@gmail.com>
2015-08-24 23:21:25 -0500

    Started implementation of laser cannon overheat functionality.

--- Speeder/LaserCannon.ino
+++ Speeder/LaserCannon.ino
@@ -1,11 +1,23 @@
-#define LASER_LIGHT_CYCLE_TIME 30    // How often the shiftbrites are updated (in Milliseconds)
-#define LASER_FADE_FACTOR      .04   // Rate that laser cannon dims (from 100 down to 0)
+#define LASER_LIGHT_CYCLE_TIME           30  // How often the shiftbrites are updated (in Milliseconds)
+#define LASER_FADE_FACTOR                .04 // Rate that laser cannon dims (from 100 down to 0)
+#define MAX_LASER_FIRES_PER_MINUTE       6   // How many times you can fire the laser cannon in a minute before it overheats
 
 void processLaserCannon(void)
 {
-  static long waitTimer;
+  static unsigned long waitTimer;
+  static unsigned long oneSecondTimer = millis();
   static float laserCannonBrightness;
+  static int laserCannonTemperature = 0;
   
+  if(millis() - oneSecondTimer > 1000)    //Process the laser cannon temperature and check for overheating
+  {  
+    oneSecondTimer = millis();
+    if(laserCannonTemperature >0)
+    {
+      laserCannonTemperature--;
+    }
+  }
+
   if(myWaveFileJustFinishedPlaying(LASER))
   {
     setVFDmessageInactive(0);
@@ -31,7 +43,14 @@ void processLaserCannon(void)
     setVFDmessageActive(0, "  Laser Cannon");
     SetVibratorMotorLeft(127);
     setLaserCannonBrightness(laserCannonBrightness);
+    laserCannonTemperature += (60 / MAX_LASER_FIRES_PER_MINUTE);
+    Serial.print("laserCannonTemperature = "); Serial.println(laserCannonTemperature);
   }
+   
+  if(laserCannonTemperature > 60)
+ {
+  Serial.println("Laser temperature too high");
+ } 
 }
 
 
